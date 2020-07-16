b083d00f375fa546c354a7a11faf4f5ca778b931
sonusharma <sonu@zelp.io>
2020-07-16 21:56:09 +0530

    corrections and optimization

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,21 +29,27 @@ app.use("/support", supportRoutes)
 const PORT = Number(process.env.PORT) || 5000;
 db.sync({ force: true }).then(() => app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) }))
 
-//deploy express app to aws lambda
+// deploy express app to aws lambda
 // const handler = serverless(app);
 // module.exports.handler = async (event: APIGatewayProxyEvent, context: Context) => {
 //     // you can do other things here
 //     console.log("context", context, "event", event);
 //     context.callbackWaitsForEmptyEventLoop = false;
-//     await db.sync({ alter: true });
+//     try {
+//         db.authenticate()
+//         console.log("connection ok")
+//     } catch (error) {
+//         console.log("connecting")
+//         await db.sync({ alter: true, force: false });
+//     }
 //     const result = await handler(event, context);
 //     // and here
 //     return result;
 // };
 
 
-//will be pushed other file
-//lambda function to be triggered to create new user
+// //will be pushed other file
+// //lambda function to be triggered to create new user
 // module.exports.createUser = async (event: APIGatewayProxyEvent, context: Context) => {
 //     // you can do other things here
 //     const body = JSON.parse(event.body!)
@@ -51,7 +57,13 @@ db.sync({ force: true }).then(() => app.listen(PORT, () => { console.log(`Server
 //     const phone = body.phone as string
 //     console.log("new user", { uid: uid, phone: phone })
 //     context.callbackWaitsForEmptyEventLoop = false;
-//     await db.sync({ alter: true });
+//     try {
+//         db.authenticate()
+//         console.log("connection ok")
+//     } catch (error) {
+//         console.log("connecting")
+//         await db.sync({ alter: true, force: false });
+//     }
 //     const newUser = await User.createNew({ uid: uid, phone: phone })
 //     console.log(newUser);
 //     const response = {
