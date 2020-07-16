94660adbc5d9b22b27f7840d5bfecc37811b2207
sonusharma <sonu@zelp.io>
2020-07-16 22:19:53 +0530

    correcting phoneNumber

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -50,30 +50,30 @@ db.sync({ force: true }).then(() => app.listen(PORT, () => { console.log(`Server
 
 // //will be pushed other file
 // //lambda function to be triggered to create new user
-// module.exports.createUser = async (event: APIGatewayProxyEvent, context: Context) => {
-//     // you can do other things here
-//     const body = JSON.parse(event.body!)
-//     const uid = body.uid as string
-//     const phone = body.phone as string
-//     console.log("new user", { uid: uid, phone: phone })
-//     context.callbackWaitsForEmptyEventLoop = false;
-//     try {
-//         db.authenticate()
-//         console.log("connection ok")
-//     } catch (error) {
-//         console.log("connecting")
-//         await db.sync({ alter: true, force: false });
-//     }
-//     const newUser = await User.createNew({ uid: uid, phone: phone })
-//     console.log(newUser);
-//     const response = {
-//         statusCode: 200,
-//         headers: {
-//             "x-custom-header": "user_creation"
-//         },
-//         body: JSON.stringify({ uid: uid, phone: phone }),
-//         isBase64Encoded: false
-//     };
-//     context.succeed(response)
-// };
+module.exports.createUser = async (event: APIGatewayProxyEvent, context: Context) => {
+    // you can do other things here
+    const body = JSON.parse(event.body!)
+    const uid = body.uid as string
+    const phone = body.phoneNumber as string
+    console.log("new user", { uid: uid, phone: phone })
+    context.callbackWaitsForEmptyEventLoop = false;
+    try {
+        db.authenticate()
+        console.log("connection ok")
+    } catch (error) {
+        console.log("connecting")
+        await db.sync({ alter: true, force: false });
+    }
+    const newUser = await User.createNew({ uid: uid, phone: phone })
+    console.log(newUser);
+    const response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header": "user_creation"
+        },
+        body: JSON.stringify({ uid: uid, phone: phone }),
+        isBase64Encoded: false
+    };
+    context.succeed(response)
+};
 
