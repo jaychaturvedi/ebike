d1678f7d12ec3ecd73fc07d57227b7bcc2f9f1b2
sonusharma <sonu@zelp.io>
2020-07-22 13:25:01 +0530

    sub header cutomization , date picker handling in dropdown

--- webapp/src/component/subHeader/index.tsx
+++ webapp/src/component/subHeader/index.tsx
@@ -10,71 +10,194 @@ import { ReactComponent as Search } from "../../assets/search_icon.svg"
 import moment from 'moment';
 interface SubHeaderProps { }
 
-interface SubHeaderStates { }
-const { Option, OptGroup } = Select;
+interface SubHeaderStates {
+    selectedVehicle: string
+    selectedLocation: string
+    vehicleActive: boolean
+    locationActive: boolean
+    selectedCalender: string
+    calenderActive: boolean
+    timeFrameVisible: boolean
+    timeFrame: {
+        starTime: string,
+        endTime: string
+    } | null
+    dateRangeApplied: boolean
+}
 
 class SubHeader extends PureComponent<SubHeaderProps, SubHeaderStates> {
-    //     function handleButtonClick(e) {
-    //     message.info('Click on left button.');
-    //     console.log('click left button', e);
-    // }
-
-    handleMenuClick = (e: any) => {
-        message.info('Click on menu item.');
-        console.log('click', e);
+    constructor(props: SubHeaderProps) {
+        super(props);
+        this.state = {
+            selectedVehicle: "Vehicle",
+            vehicleActive: false,
+            selectedLocation: "Location",
+            locationActive: false,
+            calenderActive: false,
+            selectedCalender: "Time Frame",
+            timeFrameVisible: false,
+            timeFrame: {
+                endTime: "",
+                starTime: ""
+            },
+            dateRangeApplied: false
+        }
     }
 
+    handleVehicleClick = (e: any) => {
+        message.info('Click on menu item.');
+        console.log('click', e);
+        this.setState({
+            selectedVehicle: e.key,
+            vehicleActive: true,
+            locationActive: false,
+            calenderActive: false,
+        })
+    }
+
+    handleLocationClick = (e: any) => {
+        message.info('Click on menu item.');
+        console.log('click', e);
+        this.setState({
+            selectedLocation: e.key,
+            vehicleActive: false,
+            locationActive: true,
+            calenderActive: false,
+        })
+    }
+
+    handleDateClick = (e: any) => {
+        message.info('Click on menu item.');
+        console.log('click', e);
+        switch (e.key) {
+            case "1": {
+                this.setState({
+                    selectedCalender: moment(new Date(), this.dateFormatList[0]).toLocaleString(),
+                    vehicleActive: false,
+                    locationActive: false,
+                    calenderActive: true,
+                    timeFrameVisible: false
+                });
+                break;
+            }
+            case "2": {
+                this.setState({
+                    selectedCalender: moment(new Date(), this.dateFormatList[0]).toLocaleString(),
+                    vehicleActive: false,
+                    locationActive: false,
+                    calenderActive: true,
+                    timeFrameVisible: false
+                });
+                break;
+            }
+            case "3": {
+                this.setState({
+                    selectedCalender: moment(new Date(), this.dateFormatList[0]).toLocaleString(),
+                    vehicleActive: false,
+                    locationActive: false,
+                    calenderActive: true,
+                    timeFrameVisible: false
+                });
+                break;
+            }
+        }
+    }
+
+    timeFrameVisibleChange = (open: boolean) => {
+        console.log(open)
+        this.setState({
+            timeFrameVisible: open
+        })
+    }
+
+    onFromChange = (value: any, dateString: string) => {
+        console.log('Selected Time: ', value);
+        console.log('Formatted Selected Time: ', dateString)
+        this.setState(
+            {
+                timeFrame: {
+                    starTime: dateString,
+                    endTime: this.state.timeFrame!.endTime
+                }
+            }
+        )
+    }
+    onToChange = (value: any, dateString: any) => {
+        console.log('Selected Time: ', value);
+        console.log('Formatted Selected Time: ', dateString)
+        this.setState(
+            {
+                timeFrame: {
+                    endTime: dateString,
+                    starTime: this.state.timeFrame!.starTime
+                }
+            }
+        )
+    }
+
+    timeRangeApply = () => {
+        console.log("applying date", this.state.timeFrame)
+        this.setState({
+            selectedCalender: `${this.state.timeFrame!.starTime} to ${this.state.timeFrame!.endTime}`,
+            vehicleActive: false,
+            locationActive: false,
+            calenderActive: true,
+            timeFrameVisible: false,
+            dateRangeApplied: false,
+        })
+    }
+    dateFormatList = ['DD/MM/YYYY'];
     render() {
         const vehicle = (
-            <Menu onClick={this.handleMenuClick}>
-                <Menu.Item key="1" icon={<Vehicle width="20" height="20" />}>
+            <Menu onClick={this.handleVehicleClick}>
+                <Menu.Item key="Classic" icon={<Vehicle width="20" height="20" />}>
                     <Typography.Text strong style={{ color: "#ffffff", marginLeft: "10%" }}>Classic</Typography.Text>
                 </Menu.Item>
-                <Menu.Item key="2" className={"dropdown-sub-item"}>
+                <Menu.Item key="Ice" className={"dropdown-sub-item"}>
                     Ice
                 </Menu.Item>
-                <Menu.Item key="3" className={"dropdown-sub-item"}>
+                <Menu.Item key="Kivo Standard" className={"dropdown-sub-item"}>
                     Kivo Standard
                 </Menu.Item>
-                <Menu.Item key="4" className={"dropdown-sub-item"}>
+                <Menu.Item key="Kivo Easy" className={"dropdown-sub-item"}>
                     Kivo Easy
                 </Menu.Item>
-                <Menu.Item key="5" icon={<CargoVehicle width="20" height="20" />}>
+                <Menu.Item key="Cargo" icon={<CargoVehicle width="20" height="20" />}>
                     <Typography.Text strong style={{ color: "#ffffff", marginLeft: "10%" }}>Cargo</Typography.Text>
                 </Menu.Item>
-                <Menu.Item key="4" className={"dropdown-sub-item"}>
+                <Menu.Item key="Ham" className={"dropdown-sub-item"}>
                     Ham
                 </Menu.Item>
             </Menu>
         );
         const location = (
-            <Menu onClick={this.handleMenuClick}>
-                <Menu.Item key="1">
+            <Menu onClick={this.handleLocationClick}>
+                <Menu.Item key="North">
                     <Typography.Text strong style={{ color: "#ffffff", marginLeft: "10%" }}>North</Typography.Text>
                 </Menu.Item>
-                <Menu.Item key="2">
+                <Menu.Item key="South">
                     <Typography.Text strong style={{ color: "#ffffff", marginLeft: "10%" }}>South</Typography.Text>
                 </Menu.Item>
-                <Menu.Item key="2" className={"dropdown-sub-item"}>
+                <Menu.Item key="Bangalore" className={"dropdown-sub-item"}>
                     Bangalore
                 </Menu.Item>
-                <Menu.Item key="2" className={"dropdown-sub-item"}>
+                <Menu.Item key="Hyderabad" className={"dropdown-sub-item"}>
                     Hyderabad
                 </Menu.Item>
-                <Menu.Item key="3">
+                <Menu.Item key="East">
                     <Typography.Text strong style={{ color: "#ffffff", marginLeft: "10%" }}>East</Typography.Text>
                 </Menu.Item>
-                <Menu.Item key="2" className={"dropdown-sub-item"}>
+                <Menu.Item key="kolkata" className={"dropdown-sub-item"}>
                     kolkata
                 </Menu.Item>
-                <Menu.Item key="4">
+                <Menu.Item key="West">
                     <Typography.Text strong style={{ color: "#ffffff", marginLeft: "10%" }}>West</Typography.Text>
                 </Menu.Item>
             </Menu>
         );
-        const dateFormatList = ['DD/MM/YYYY'];
+
         const timeFrame = (
-            <Menu onClick={this.handleMenuClick}>
+            <Menu onClick={this.handleDateClick}>
                 <Menu.Item key="1" style={{ marginLeft: "5%" }}>
                     As of Today
                 </Menu.Item>
@@ -84,10 +207,13 @@ class SubHeader extends PureComponent<SubHeaderProps, SubHeaderStates> {
                 <Menu.Item key="3" style={{ marginLeft: "5%" }}>
                     Month Till Date
                 </Menu.Item>
-                <Menu.Item key="4" style={{ marginLeft: "5%" }}>
-                    Date Range
-                    <DatePicker defaultValue={moment('01/01/2015', dateFormatList[0])} format={dateFormatList} bordered={false} />
-                    <DatePicker defaultValue={moment('01/01/2015', dateFormatList[0])} format={dateFormatList} bordered={false} />
+                <Menu.Item key="4" style={{ marginLeft: "5%" }} disabled={true}>
+                    <Typography.Text strong style={{ color: "#ffffff", whiteSpace: "nowrap" }}>Date Range</Typography.Text>
+                    <DatePicker onChange={this.onFromChange} defaultValue={moment()} format={this.dateFormatList} bordered={false} />
+                    <DatePicker onChange={this.onToChange} defaultValue={moment()} format={this.dateFormatList} bordered={false} />
+                    <Button size={"small"} className={"apply-button"} onClick={this.timeRangeApply}>
+                        APPLY
+                    </Button>
                 </Menu.Item>
             </Menu>
         );
@@ -97,29 +223,33 @@ class SubHeader extends PureComponent<SubHeaderProps, SubHeaderStates> {
                     ALL
                 </Button>
                 <Dropdown overlay={vehicle} trigger={['click']}>
-                    <div className={"connectM-dropDown"}>
-                        <div className={"pair"} >
-                            <Vehicle width="20" height="20" />
-                            <Typography.Text style={{ color: "#ffffff", marginLeft: "10%" }}>Vehicle</Typography.Text>
+                    <div className={`connectM-dropDown ${this.state.vehicleActive ? "connectM-dropdown-active" : ""}`}>
+                        <div className={"pair "} >
+                            <Vehicle width="20" height="20" className={`dropdown-svg-fill ${this.state.vehicleActive ? "dropdown-svg-fill-active" : ""}`} />
+                            <Typography.Text className={`dropdown-typography ${this.state.vehicleActive ? "typography-active" : ""}`}>{this.state.selectedVehicle}</Typography.Text>
                         </div>
-                        <DownOutlined style={{ marginLeft: "20%" }} />
+                        <DownOutlined className={"flip"} style={{ marginLeft: "40px" }} />
                     </div>
                 </Dropdown>
                 <Dropdown overlay={location} trigger={['click']}>
-                    <div className={"connectM-dropDown"}>
-                        <div className={"pair"} >
-                            <Location width="20" height="20" />
-                            <Typography.Text style={{ color: "#ffffff", marginLeft: "10%" }}>Location</Typography.Text>
+                    <div className={`connectM-dropDown ${this.state.locationActive ? "connectM-dropdown-active" : ""}`}>
+                        <div className={"pair "} >
+                            <Location width="20" height="20" className={`dropdown-svg-fill-location ${this.state.locationActive ? "dropdown-svg-fill-location-active" : ""}`} />
+                            <Typography.Text className={`dropdown-typography ${this.state.locationActive ? "typography-active" : ""}`}>{this.state.selectedLocation}</Typography.Text>
                         </div>
-                        <DownOutlined style={{ marginLeft: "20%" }} /></div>
+                        <DownOutlined className={"flip"} style={{ marginLeft: "40px" }} />
+                    </div>
                 </Dropdown>
-                <Dropdown overlay={timeFrame} trigger={['click']}>
-                    <div className={"connectM-dropDown"}>
-                        <div className={"pair"} >
-                            <Calender width="25" height="20" />
-                            <Typography.Text style={{ color: "#ffffff", marginLeft: "10%", whiteSpace: "nowrap" }}>Time Frame</Typography.Text>
+                <Dropdown overlay={timeFrame} trigger={['click']}
+                    visible={this.state.timeFrameVisible}
+                    onVisibleChange={this.timeFrameVisibleChange}
+                >
+                    <div className={`connectM-dropDown ${this.state.calenderActive ? "connectM-dropdown-active" : ""}`}>
+                        <div className={"pair "} >
+                            <Calender width="20" height="20" className={`dropdown-svg-fill-timeframe ${this.state.calenderActive ? "dropdown-svg-fill-timeframe-active" : ""}`} />
+                            <Typography.Text className={`dropdown-typography ${this.state.calenderActive ? "typography-active" : ""}`}>{this.state.selectedCalender}</Typography.Text>
                         </div>
-                        <DownOutlined style={{ marginLeft: "20%" }} />
+                        <DownOutlined className={"flip"} style={{ marginLeft: "40px" }} />
                     </div>
                 </Dropdown>
                 <div style={{ width: "200px" }} className={"search-background-color"}>
