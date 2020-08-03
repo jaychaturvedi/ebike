96b21a2042f77363f2da523177dbd4e83fe2daf1
Jay Chaturvedi <jay@zelp.io>
2020-08-03 14:38:54 +0530

    styled line graph

--- webapp/src/component/rnd-alert-detail/alert-detail-graph/index.tsx
+++ webapp/src/component/rnd-alert-detail/alert-detail-graph/index.tsx
@@ -66,51 +66,53 @@ class AlertDetailGraph extends PureComponent<AlertDetailGraphProps, AlertDetailG
                     <Typography.Text style={{ color: "#ffffff", fontSize: '15px' }} strong>Low Mileage</Typography.Text>
                 </div>
                 {/* <LineGraph/> */}
-                <ResponsiveContainer width="95%" height="100%">
-                    <LineChart
-                        data={data}
-                        margin={{
-                            top: 0, right: 0, left: 0, bottom: 0,
-                        }}
-                    >
-                        <Legend wrapperStyle={{ top: -18, left: 30 }} iconType="circle" iconSize={10} />
-                        <CartesianGrid strokeDasharray="3 3 5 2" stroke="#515151" />
-                        <ReferenceLine y={35} stroke="green" strokeDasharray="3 3 5 2"
-                            isFront={true} >
-                            <Label position={'insideBottomLeft'} fill="#ffffff"
-                                style={{
-                                    fontSize: '8px', textAnchor: 'center', fontFamily: 'Roboto'
-                                }} value="L1">
-                            </Label>
-                        </ReferenceLine>
-                        <ReferenceLine y={25} stroke="green" strokeDasharray="3 3 5 2"
-                            isFront={true} >
-                            <Label position={'insideTopLeft'} fill="#ffffff"
-                                style={{
-                                    fontSize: '8px', textAnchor: 'center', fontFamily: 'Roboto'
-                                }} value="L2">
-                            </Label>
-                        </ReferenceLine>
-                        {/* <XAxis orientation='top' stroke='#ffffff' tick={false} /> */}
-                        <XAxis dataKey="nocycles" tick={{ fill: 'white' }} stroke='#ffffff' interval="preserveEnd" padding={{ left: 30, right: 20 }}>
-                            <Label
-                                value="No. of Cycles"
-                                position="bottom"
-                                offset={0}
-                                style={{ padding: 5 }}
-                                content={props => { return this.DynamicLabel(props) }}
-                            />                        </XAxis>
-                        <YAxis tick={{ fill: 'white' }} padding={{ top: 20, bottom: 30 }} domain={[5, 50]} stroke='#ffffff'>
-                            <Label angle={270} position='left' offset={-25} fill="#ffffff"
-                                style={{
-                                    fontSize: '12px', textAnchor: 'middle', fontFamily: 'Roboto'
-                                }} value="Mileage (KM)">
-                            </Label>
-                        </YAxis>
-                        <Line name="Specified Mileage" type="monotone" dataKey="smilage" stroke="#8884d8" dot={{ r: 1 }} strokeWidth={3} />
-                        <Line name="Actual Mileage" type="monotone" dataKey="amilage" stroke="#82ca9d" strokeWidth={3} dot={{ r: 1 }} />
-                    </LineChart>
-                </ResponsiveContainer>
+                <div style={{ display: 'flex', justifyContent: 'center', height: '100%', width: '100%' }} >
+                    <ResponsiveContainer width="95%" height="95%">
+                        <LineChart
+                            data={data}
+                            margin={{
+                                top: 0, right: 0, left: 0, bottom: 0,
+                            }}
+                        >
+                            <Legend wrapperStyle={{ top: -18, left: 30 }} iconType="circle" iconSize={10} />
+                            <CartesianGrid strokeDasharray="3 3 5 2" stroke="#515151" />
+                            <ReferenceLine y={35} stroke="green" strokeDasharray="3 3 5 2"
+                                isFront={true} >
+                                <Label position={'insideBottomLeft'} fill="#ffffff"
+                                    style={{
+                                        fontSize: '8px', textAnchor: 'center', fontFamily: 'Roboto'
+                                    }} value="L1">
+                                </Label>
+                            </ReferenceLine>
+                            <ReferenceLine y={25} stroke="green" strokeDasharray="3 3 5 2"
+                                isFront={true} >
+                                <Label position={'insideTopLeft'} fill="#ffffff"
+                                    style={{
+                                        fontSize: '8px', textAnchor: 'center', fontFamily: 'Roboto'
+                                    }} value="L2">
+                                </Label>
+                            </ReferenceLine>
+                            {/* <XAxis orientation='top' stroke='#ffffff' tick={false} /> */}
+                            <XAxis dataKey="nocycles" tick={{ fill: 'white' }} stroke='#ffffff' interval="preserveEnd" padding={{ left: 30, right: 20 }}>
+                                <Label
+                                    value="No. of Cycles"
+                                    position="bottom"
+                                    offset={0}
+                                    style={{ padding: 5 }}
+                                    content={props => { return this.DynamicLabel(props) }}
+                                />                        </XAxis>
+                            <YAxis tick={{ fill: 'white' }} padding={{ top: 20, bottom: 30 }} domain={[5, 50]} stroke='#ffffff'>
+                                <Label angle={270} position='left' offset={-25} fill="#ffffff"
+                                    style={{
+                                        fontSize: '12px', textAnchor: 'middle', fontFamily: 'Roboto'
+                                    }} value="Mileage (KM)">
+                                </Label>
+                            </YAxis>
+                            <Line name="Specified Mileage" type="monotone" dataKey="smilage" stroke="#8884d8" dot={{ r: 1 }} strokeWidth={3} />
+                            <Line name="Actual Mileage" type="monotone" dataKey="amilage" stroke="#82ca9d" strokeWidth={3} dot={{ r: 1 }} />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
 
             </div >
         )
