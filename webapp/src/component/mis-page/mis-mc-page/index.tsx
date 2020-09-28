1624f36f1fd3b5dcc8187f25284c72c46a3cd151
killbee <jay@zelp.io>
2020-09-28 08:48:35 +0530

    added header for battery

--- webapp/src/component/mis-page/mis-mc-page/index.tsx
+++ webapp/src/component/mis-page/mis-mc-page/index.tsx
@@ -32,91 +32,115 @@ class MisMC extends PureComponent<Props, States> {
                 {/* <Header /> */}
                 <div className="grid-container">
                     <div className="item1 top-cell left">
-                        <div className="dialer">
-                            <div style={{
-                                width: "30%", margin: '0 auto', position: 'relative',
-                                top: '24px'
-                            }}>
-                                <Dialer />
+                        <div className="header">
+                            <div className='last-sync-header'>
+                                <div className="text-pair left">
+                                    Last Synced On
+                                </div>
+                                <div className="text-pair right">
+                                    1 Aug 20209:00AM
+                        </div>
+                            </div> </div>
+                        <div className="content">
+                            <div className="dialer">
+                                <div style={{
+                                    width: "30%", margin: '0 auto', position: 'relative',
+                                    top: '24px'
+                                }}>
+                                    <Dialer />
+                                </div>
+                            </div>
+                            <div className="dialer"></div>
+                            <div className="card-container">
+                                <span>Soc</span>
+                                <div className="card-body">
+                                    <div className="label">
+                                        <div>
+                                            <Typography.Text style={{ whiteSpace: "nowrap", color: 'grey' }}>Charge</Typography.Text>
+                                        </div>
+                                        <div>
+                                            <Typography.Text style={{ whiteSpace: "nowrap" }} strong>84%</Typography.Text>
+                                        </div>
+                                    </div>
+                                    <div className="label">
+                                        <div>
+                                            <Typography.Text style={{ whiteSpace: "nowrap", color: 'grey' }}>Pressure</Typography.Text>
+                                        </div>
+                                        <div>
+                                            <Typography.Text style={{ whiteSpace: "nowrap" }} strong>2.3V</Typography.Text>
+                                        </div>
+                                    </div>
+
+
+                                    <div className="label" >
+                                        <div>
+                                            <Typography.Text style={{ whiteSpace: "nowrap", color: 'grey' }}>Acquisition</Typography.Text>
+                                        </div>
+                                        <div>
+                                            <Typography.Text style={{ whiteSpace: "nowrap", color: 'red' }} strong>2.2V</Typography.Text>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
-                        <div className="dialer"></div>
-                        <div className="card-container">
-                            <span>Soc</span>
-                            <div className="card-body">
-                                <div className="label">
-                                    <div>
-                                        <Typography.Text style={{ whiteSpace: "nowrap", color: 'grey' }}>Charge</Typography.Text>
-                                    </div>
-                                    <div>
-                                        <Typography.Text style={{ whiteSpace: "nowrap" }} strong>84%</Typography.Text>
-                                    </div>
-                                </div>
-                                <div className="label">
-                                    <div>
-                                        <Typography.Text style={{ whiteSpace: "nowrap", color: 'grey' }}>Pressure</Typography.Text>
-                                    </div>
-                                    <div>
-                                        <Typography.Text style={{ whiteSpace: "nowrap" }} strong>2.3V</Typography.Text>
-                                    </div>
-                                </div>
-
-
-                                <div className="label" >
-                                    <div>
-                                        <Typography.Text style={{ whiteSpace: "nowrap", color: 'grey' }}>Acquisition</Typography.Text>
-                                    </div>
-                                    <div>
-                                        <Typography.Text style={{ whiteSpace: "nowrap", color: 'red' }} strong>2.2V</Typography.Text>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-
                     </div>
                     <div className="item2 top-cell right">
-                        <div className="battery-table">
-                            <CustomizedTables />
+                        <div className="header">
+                            <Typography.Text style={{ whiteSpace: "nowrap" }} className="header-title" strong>BATTERY STATUS</Typography.Text>
                         </div>
-                        <div className="health">
-                            <div className="health-tab">
-                                <div className="health-card" >
-                                    <Typography.Text style={{ whiteSpace: "normal" }}>Ambient Temperature</Typography.Text>
-                                    <Typography.Text style={{ whiteSpace: "nowrap" }}>24' C</Typography.Text>
-                                </div>
-                                <div className="health-card" >
-                                    <Typography.Text style={{ whiteSpace: "normal" }}>Ambient Temperature</Typography.Text>
-                                    <Typography.Text style={{ whiteSpace: "nowrap" }}>24' C</Typography.Text>
-                                </div>
+                        <div className="content">
+                            <div className="battery-table">
+                                <CustomizedTables />
                             </div>
-                            <div className="battery-image-container">
-                                <div className="battery-health-circle">
-                                    <img src={Battery} style={{ width: '50px', height: '50px', alignSelf: 'center' }} />
-                                    <div style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }} >
-                                        <Typography.Text style={{ whiteSpace: "normal", }} strong>Health</Typography.Text>
-                                        <Typography.Text style={{ whiteSpace: "nowrap" }} strong>82%</Typography.Text>
+                            <div className="health">
+                                <div className="health-tab">
+                                    <div className="health-card" >
+                                        <Typography.Text style={{ whiteSpace: "normal" }}>Ambient Temperature</Typography.Text>
+                                        <Typography.Text style={{ whiteSpace: "nowrap" }}>24' C</Typography.Text>
+                                    </div>
+                                    <div className="health-card" >
+                                        <Typography.Text style={{ whiteSpace: "normal" }}>Ambient Temperature</Typography.Text>
+                                        <Typography.Text style={{ whiteSpace: "nowrap" }}>24' C</Typography.Text>
                                     </div>
                                 </div>
+                                <div className="battery-image-container">
+                                    <div className="battery-health-circle">
+                                        <img src={Battery} style={{ width: '50px', height: '50px', alignSelf: 'center' }} />
+                                        <div style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }} >
+                                            <Typography.Text style={{ whiteSpace: "normal", }} strong>Health</Typography.Text>
+                                            <Typography.Text style={{ whiteSpace: "nowrap" }} strong>82%</Typography.Text>
+                                        </div>
+                                    </div>
 
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="item3 bottom-cell left">
-
+                        <div className="header">
+                            <Typography.Text style={{ whiteSpace: "nowrap" }} className="header-title" strong>CELL VOLTAGE</Typography.Text>
+                        </div>
+                        <div className="content"></div>
                         {/* <div className="graph-container">
                             <ChargingTrend />
                         </div> */}
                     </div>
                     <div className="item4 bottom-cell center">
+                        <div className="header">
+                            <Typography.Text style={{ whiteSpace: "nowrap" }} className="header-title" strong>LAST CHARGING TREND</Typography.Text>
+
+                        </div>
 
                         <div className="graph-container">
                             <ChargingTrend />
                         </div>
                     </div>
                     <div className="item5 bottom-cell right">
+                        <div className="header">
+                            <Typography.Text style={{ whiteSpace: "nowrap" }} className="header-title" strong>LAST DISCHARGING TREND</Typography.Text>
 
-                        <div className="graph-container">
+                        </div>                        <div className="graph-container">
                             <ChargingTrend />
                         </div>
                     </div>
