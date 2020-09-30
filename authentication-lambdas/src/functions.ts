9dbd25c42d16636ff6273741acf244c967fab1df
sonusharma <sonu@zelp.io>
2020-09-30 22:50:44 +0530

    create webapp user and custom forgot password trigger

--- authentication-lambdas/src/functions.ts
+++ authentication-lambdas/src/functions.ts
@@ -135,3 +135,28 @@ module.exports.postConfirmation = async (event: any) => {
 //     }
 //     return event;
 // };
+
+module.exports.webAppForgotPassword = async (event: any) => {
+    console.log("forgot password",event);
+    var CustomMessage_ForgotPassword = `<style>
+        p {
+        display: block;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        }
+        </style>
+
+        <div id=":x9" class="a3s aXjCH " role="gridcell" tabindex="-1"><p>Hello,</p>
+        <p>Follow this link to reset your Password. </p>
+        <p><a href="${process.env.WEBAPPSITEURL}/reset-password?confirmation_code=${event.request.codeParameter}&user_name=${event.userName}"> Reset Password </a></p>
+        <p>If you didn’t ask to change password, you can ignore this email.</p>
+        <p>Thanks,</p>
+        <p>Your website team</p>
+        </div>`
+    if (event.triggerSource === "CustomMessage_ForgotPassword") {
+        event.response.emailMessage = CustomMessage_ForgotPassword;
+    }
+    return event;
+};
