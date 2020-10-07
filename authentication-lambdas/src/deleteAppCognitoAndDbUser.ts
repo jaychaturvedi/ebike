e60e951c568077139adf09765328a75f76feb932
killbee <jay@zelp.io>
2020-10-07 19:22:32 +0530

    delete Cognito and db user api

--- authentication-lambdas/src/deleteAppCognitoAndDbUser.ts
+++ authentication-lambdas/src/deleteAppCognitoAndDbUser.ts
@@ -2,17 +2,15 @@ const AWS = require('aws-sdk');
 import * as rp from 'request-promise'
 import * as dotenv from "dotenv"
 import { APIGatewayProxyEvent, Context } from "aws-lambda";
-dotenv.config()
 const globalAny: any = global;
 globalAny.fetch = require("node-fetch");
 globalAny.navigator = () => null
-
 const Amplify = require("@aws-amplify/core").default;
 const Auth = require("@aws-amplify/auth").default;
-
 const userPoolRegion = 'us-east-2'
-const userPoolID = 'us-east-2_3ErdY8hH0';
+const userPoolID = 'us-east-2_3ErdY8hH0';//zelp_app_authentication
 const userPoolWebClientID = '201rvp9cga1v0foim2aab6g3of';
+dotenv.config()
 const accessKeyId = process.env.ACCESSKEYID
 const secretAccessKey = process.env.SECRETKEY
 const region = "us-east-2"
@@ -31,8 +29,6 @@ var CognitoIdentityServiceProvider = AWS.CognitoIdentityServiceProvider;
 var client = new CognitoIdentityServiceProvider({ apiVersion: '2016-04-19' });
 
 function deleteUser(params: any) {
-    console.log(process.env);
-
     let options = {
         method: 'DELETE',
         uri: process.env.MOBILEAPIURL + params.Username,
@@ -44,46 +40,29 @@ function deleteUser(params: any) {
 
         //delete from cognito pool
         client.adminDeleteUser(params).promise()
-            .then((user: any) => {
+            .then(() => {
                 console.warn(".. delete_cognito: complete");
                 return rp(options)
             })
             .then((response: any) => {
-                console.log("DELETE succeeded with status %d", response.statusCode, response.body);
-                resolve(response.body)
+                console.log("DELETE succeeded with status ", response);
+                const { status, body, error, date } = JSON.parse(response.body)
+                resolve({
+                    status, body,
+                    error, date
+                })
             })
             .catch((err: any) => {
                 console.error(".. delete_cognito: error");
-                console.error(err);
-                reject(err);
+                console.error(err.code, err.message, err.statusCode);
+                reject({ code: err.code, message: err.message, statusCode: err.statusCode });
             });
-
-        //delete from database
-        // rp(options)
-        //     .then(function (response) {
-        //         console.log("DELETE succeeded with status %d", response.statusCode, response.body);
-        //         resolve(response.body)
-        //     })
-        //     .catch((err) => {
-        //         reject(err)
-        //         console.log(err);
-        //     });
     })
 }
-var paramsy = {
-    UserPoolId: 'us-east-2_4yqT9fdQs',
-    Username: '+917478523376',
-}
-deleteUser(paramsy)
-    .then((userDetails) => {
-        console.log('user succesfull deleted from cognito and db', userDetails)
-    })
-    .catch(err => console.log(err.code, err.message, err.statusCode));
-
 
 // will be pushed other file
 // lambda function to be triggered to create new user
-module.exports.deleteMobileCognitoUser = async (event: APIGatewayProxyEvent, context: Context) => {
+module.exports.deleteAppCognitoAndDbUser = async (event: APIGatewayProxyEvent, context: Context) => {
     context.callbackWaitsForEmptyEventLoop = false;
     const body = JSON.parse(event.body!)
     const phone = body.phone
@@ -92,7 +71,7 @@ module.exports.deleteMobileCognitoUser = async (event: APIGatewayProxyEvent, con
         Username: phone
     };
     let responseBody: any
-    // let deletedDbUser: any
+    let deletedDbUser: any
     let errorBody: any
     try {
         responseBody = await deleteUser(params)
@@ -100,7 +79,12 @@ module.exports.deleteMobileCognitoUser = async (event: APIGatewayProxyEvent, con
         console.log(responseBody)
     }
     catch (e) {
-        console.log(e)
+        deletedDbUser = await rp({
+            method: 'DELETE',
+            uri: process.env.MOBILEAPIURL + params.Username,
+            resolveWithFullResponse: true
+        })
+        console.log(deletedDbUser);
         errorBody = e
     }
     const response = {
