41e2b5ae38b65276670c26ab7f99940a6c87937c
killbee <jay@zelp.io>
2020-11-03 13:54:31 +0530

    added filters in map view

--- webapp/src/component/googleMap/marker.tsx
+++ webapp/src/component/googleMap/marker.tsx
@@ -3,17 +3,18 @@ import './marker.scss';
 import { Popover, Button } from 'antd';
 
 const content = (props: any) => (
-  <div style={{fontSize:"13px"}}>
-    <p>FrameId = {props.frameId}</p>
-    <p>CustomerId = {props.name}</p>
+  <div style={{fontSize:"13px"}} className="map-marker-tooltip">
+    <p>Vehicle Id : <br/>{props.frameId}</p>
+    <p>Last Active : <br/>{props.lastActive}</p>
   </div>
 );
+
 
 const Marker = (props: any) => {
   const { color, name, id } = props;
   return (
     <div>
-      <Popover content={() => content(props)} trigger="click">
+      <Popover content={() => content(props)} trigger="click" className="map-tooltip">
         <div
           className="pin bounce"
           style={{ backgroundColor: color, cursor: 'pointer' }}
