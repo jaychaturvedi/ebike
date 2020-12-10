a7281a1680c3f549af751f1adb1b9fa0d1976e12
Mohammed Muzzammil <muzzammilmohammed32@gmail.com>
2020-12-10 15:37:58 +0530

    Added charging status

--- motovoltApp/src/screens/charging.tsx
+++ motovoltApp/src/screens/charging.tsx
@@ -1,81 +1,43 @@
-import React from 'react'
-import { View, StyleSheet, Image } from 'react-native'
-import { moderateScale } from 'react-native-size-matters'
-import ChargingTile from '../components/charging-tile'
-import Background from '../components/background'
-import Header from './home/components/header/index'
-import Colors from '../styles/colors'
+import React from 'react';
+import {Text, View, Dimensions} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import ChargingStatusBG from '../assets/svg/charging-status-bg';
+import ChargingStatusCharging from '../assets/svg/charging-status-charging';
+import ChargingStatusCharged from '../assets/svg/charging-status-charged';
+import {scale, verticalScale} from '../styles/size-matters';
 
-interface Props {
-    chargingStatus: string,
-    charge: string,
-    remainingTime: string
-}
-type State = {}
+const {width, height} = Dimensions.get('window');
 
-export default class Charging extends React.PureComponent<Props, State>{
-    render() {
-        return (
-            <View style={styles.container}>
-                <Background />
-                <Header
-                    backgroundColor={Colors.HEADER_YELLOW}
-                    title={"Charging"}
-                    subtitle={"Cycle A"}
-                    hasSubtitle
-                    hasTabs
-                />
-                <View style={styles.cycle}>
-                    <Image source={require('../assets/images/cycle.png')}
-                        style={{
-                            resizeMode: 'contain',
-                            aspectRatio: 1.5, flex: 1
-                        }}
-                    />
-                </View>
-                <View style={styles.charge}>
-                    <ChargingTile
-                        icon={require("../assets/icons/charge_status.png")}
-                        metric={this.props.charge}
-                        unit={"%"}
-                        helpText={`Battery Status: ${this.props.chargingStatus}`}
-                        textColor="#2ECC71"
-                    />
-                </View>
-
-                <View style={styles.remainingTime}>
-                    <ChargingTile
-                        icon={require("../assets/icons/charge_time_large.png")}
-                        metric={this.props.remainingTime}
-                        unit={"(10 pm)"}
-                        helpText={`Remaining Time to full charge`}
-                        textColor="black"
-                    />
-                </View>
-            </View>
-        )
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
+export default function Charging() {
+  return (
+    <View
+      style={{
+        width: '100%',
         height: '100%',
-        backgroundColor: '#F0F0F0',
-    },
-    cycle: {
-        height: moderateScale(265),
-        padding: moderateScale(15),
+        backgroundColor: 'blue',
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    charge: {
-        height: moderateScale(157),
-        marginTop: moderateScale(13),
-        marginHorizontal: moderateScale(15),
-    },
-    remainingTime: {
-        height: moderateScale(157),
-        marginTop: moderateScale(20),
-        marginHorizontal: moderateScale(15),
-    }
-})  
+      }}>
+      <ChargingStatusBG
+        style={{position: 'absolute'}}
+        width={width}
+        height={height}
+        viewBox={`0 0 ${width} ${height}`}
+      />
+      <LinearGradient
+        style={{
+          height: scale(270),
+          width: scale(270),
+          borderRadius: scale(270) / 2,
+          marginBottom: verticalScale(100),
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        start={{x: 0.5, y: 0}}
+        end={{x: 0.5, y: 1}}
+        colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.3)']}>
+        <ChargingStatusCharging />
+      </LinearGradient>
+    </View>
+  );
+}
